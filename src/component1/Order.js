1f6ecde15eebaa7389e0057018c30d6f55ebf779
Arthiclaks <chitraraveendran.com>
2023-05-29 09:26:58 +0530

    planner bom updated

--- src/component1/Order.js
+++ src/component1/Order.js
@@ -3,8 +3,18 @@ import Banner from './Banner';
 import Gridlay from './BOMDetails';
 import './CustomStyles.css';
 import DropdownWithOther from './Material';
+import InputWithOther from './Powdercoating';
+import CheckboxExample from './metalstamp';
 
 function Order() {
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const options = ['Assembly','Non-Assembly','Sub-parts']
+    const option1 = ['Sheet Metal', 'Fastener']
+    const handleSelectionChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedOption(selectedValue);
+    }
     
   return (
     <><div>
@@ -71,8 +81,60 @@ function Order() {
             </div>
 
             <div class="col">
+                <InputWithOther/>
+            </div>
+    
+
+            <div class="col">
                 <div className="input-group input-group-sm">
-                    <label className="col-sm-6 col-form-label">Qty in Drawing</label>
+                    <label class="col-sm-6 col-form-label">Assembly?</label>
+                    <div class="col-sm-6">
+                    <select class="form-control" value={selectedOption} onChange={handleSelectionChange}>
+                        {options.map((option) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                    </div>
+                </div>     
+            </div>
+
+
+            <div class="col">
+                <div className="input-group input-group-sm">
+                    <label class="col-sm-6 col-form-label">Commodity</label>
+                    <div class="col-sm-6">
+                    <select class="form-control" value={selectedOption} onChange={handleSelectionChange}>
+                        {option1.map((option) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                    </div>
+                </div>     
+            </div>
+
+
+            <div class="col">
+                <div className="input-group input-group-sm">
+                    <label className="col-sm-6 col-form-label">Source</label>
+                    <div className="col-sm-6">
+                        <input  type='text'  class="form-control custom-textfield" placeholder=' '/>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+
+        
+
+        <div class="row">
+            
+        <div class="col">
+                <div className="input-group input-group-sm">
+                    <label className="col-sm-6 col-form-label">Bends</label>
                     <div className="col-sm-6">
                         <input type='number' class="form-control custom-textfield" placeholder='1'/>
                 </div>
@@ -80,33 +142,45 @@ function Order() {
             </div>
 
             <div class="col">
-                <div className="input-group input-group-sm">
-                    <label className="col-sm-6 col-form-label">Thickness</label>
-                    <div className="col-sm-6">
-                        <input  type='number'  step="0.1" class="form-control custom-textfield" placeholder='1.00'/>
+            <div className="input-group input-group-sm">
+                    <label class="col-sm-6 col-form-label">Metal Stamp</label>
+                    <div class="col-sm-6">
+                        <CheckboxExample/>
                     </div>
-                </div>
+            </div>  
+            </div>
+    
+
+            <div class="col">
+            <div className="input-group input-group-sm">
+                    <label class="col-sm-6 col-form-label">Tapping</label>
+                    <div class="col">
+                        <CheckboxExample/>
+                    </div>
+            </div> 
             </div>
 
 
             <div class="col">
-                <div className="input-group input-group-sm">
-                    <label className="col-sm-6 col-form-label">Width</label>
-                    <div className="col-sm-6">
-                        <input  type='number' step="0.1" class="form-control custom-textfield" placeholder='1.00'/>
+            <div className="input-group input-group-sm">
+                    <label class="col-sm-6 col-form-label">Welding</label>
+                    <div class="col-sm-6">
+                        <CheckboxExample/>
                     </div>
-                </div>
+            </div>     
             </div>
 
+
             <div class="col">
-                <div className="input-group input-group-sm">
-                    <label className="col-sm-6 col-form-label">Length</label>
-                    <div className="col-sm-6">
-                        <input  type='number'  step="0.1" class="form-control custom-textfield" placeholder='1.00'/>
+            <div className="input-group input-group-sm">
+                    <label class="col-sm-6 col-form-label">Clinching/Riveting</label>
+                    <div class="col-sm-6">
+                        <CheckboxExample/>
                     </div>
-                </div>
             </div>
-          
+            </div>
+
+
         </div>
         
     </form>
